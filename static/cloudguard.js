e171aaa7171ae24ba985d1692cb06f17e9fae960
Rudraprasad Mohapatra <98683883+Rudraprasad-Mohapatra@users.noreply.github.com>
2024-01-04 12:48:39 +0000

    Working tabs

--- static/cloudguard.js
+++ static/cloudguard.js
@@ -1,20 +1,21 @@
-window.onload = () => {
-  console.log("Page loaded");
-    const serachValue = document.getElementById("searchBox").value;
-    const data = {
-            "text":serachValue+""
-        }
-  const generateProgram = (data) => {
-    axios
-      .post(
-        "https://urban-chainsaw-9gqp4p59gv729pvx-5000.app.github.dev/askme",
-        data
-      )
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error making API request:", error);
-      });
+// window.onload = () => {
+
+function generateProgram(e) {
+  e.preventDefault();
+  let serachValue = document.getElementById("searchBox").value;
+  let data = {
+    text: serachValue + "",
   };
-};
+
+  axios
+    .post(
+      "https://urban-chainsaw-9gqp4p59gv729pvx-5000.app.github.dev/askme",
+      data
+    )
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error("Error making API request:", error);
+    });
+}
