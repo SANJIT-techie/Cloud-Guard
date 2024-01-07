d115c3a0fd9ecee5383524475a79335f76b5540d
Rudraprasad Mohapatra <98683883+Rudraprasad-Mohapatra@users.noreply.github.com>
2024-01-07 20:31:34 +0000

    Added Tab effect and rendering of various format of data

--- static/cloudguard.js
+++ static/cloudguard.js
@@ -1,4 +1,7 @@
-// window.onload = () => {
+allPres = document.getElementsByTagName("pre");
+for (let i = 0; i < allPres.length; i++) {
+  allPres[i].style.display = "none";
+}
 
 function generateProgram(e) {
   e.preventDefault();
@@ -9,16 +12,67 @@ function generateProgram(e) {
 
   axios
     .post(
-      "https://urban-chainsaw-9gqp4p59gv729pvx-5000.app.github.dev/askme",
+      "https://obscure-waddle-xg9jqjp55xxcvq-5000.app.github.dev/askme",
       data
     )
     .then((response) => {
-      const terraformCode = response.data;
-      console.log(response.data.res.terraform);
-      const jsonString = JSON.stringify(response.data.res.terraform, null, 2);
-      document.getElementById("code").innerText = jsonString;
+      console.log(response.data.res);
+
+      const portalString = response.data.res.Portal;
+
+      const terminalString = response.data.res.terminal;
+
+      const terraformString = JSON.stringify(
+        response.data.res.terraform,
+        null,
+        2
+      );
+
+      const referencesString = JSON.stringify(
+        response.data.res.references,
+        null,
+        2
+      );
+
+      document.getElementById("portal").innerHTML = portalString.replace(
+        /\n/g,
+        "<br>"
+      );
+
+      document.getElementById("terminal").innerHTML = terminalString;
+
+      document.getElementById("terraform").innerText = terraformString;
+
+      document.getElementById("references").innerText = referencesString;
     })
     .catch((error) => {
       console.error("Error making API request:", error);
     });
+
+  tabContent = document.getElementsByClassName("tabcontent");
+  tabContent[0].style.display = "block";
+  tabContent[0].parentElement.style.display = "block";
+  tabContent[0].className += " active";
+}
+
+function openTab(event, tabName) {
+  let i, tabContent, tablinks;
+  tabContent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+    tabContent[i].parentElement.style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName).parentElement.style.display = "block";
+
+  event.currentTarget.className += " active";
+  console.log(event.currentTarget);
 }
